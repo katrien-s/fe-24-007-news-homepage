49bfc4b135f5e9429beff0f34ceae612856972bd
Katrien S <ontwerp@katriens.be>
2026-03-27 10:53:34 +0100

    Going through FEM error report again

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-document
-	.querySelector('.hamburger-menu input')
-	.addEventListener('change', function () {
+const hamburgerInput = document.querySelector('.hamburger-menu input');
+
+if (hamburgerInput) {
+	hamburgerInput.addEventListener('change', function () {
 		const isExpanded = this.checked;
 		this.setAttribute('aria-expanded', isExpanded);
 	});
+}
